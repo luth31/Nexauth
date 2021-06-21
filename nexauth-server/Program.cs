bdaf7a6efade1bb68a14afbefb4b63f969fb15bd
Paul Munteanu <luth@luthcode.net>
2021-06-21 04:36:18 +0300

    Initial commit

---
+++ nexauth-server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nexauth_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
