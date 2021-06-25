65fcc83dae47e6442f55ba6442b66f80fa8c9cfa
Paul Munteanu <luth@luthcode.net>
2021-06-25 12:57:24 +0300

    Add basic Listener

---
+++ nexauth-server/Listener.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using nexauth;
+
+namespace nexauth_server {
+    class Listener {
+        public Listener(string address, Int32 port) {
+            IPAddress addr;
+            IPHostEntry entry = Dns.GetHostEntry(address);
+            if (entry.AddressList.Length > 0)
+                addr = entry.AddressList[0];
+            else {
+                if (!IPAddress.TryParse(address, out addr)) {
+                    Console.WriteLine($"'{address}' cannot be parsed as domain or IP!");
+                    Environment.Exit(-1);
+                }
+            }
+            listener = new TcpListener(addr, port);
+        }
+
+        public void Start() {
+            listener.Start();
+            Console.WriteLine($"Listening on {listener.LocalEndpoint}");
+            Console.WriteLine("Awaiting connection...");
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine("Client connected!");
+        }
+
+        private readonly TcpListener listener;
+    }
+}
--- nexauth-server/Program.cs
+++ nexauth-server/Program.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 
-namespace nexauth_server
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace nexauth_server {
+    class Program {
+        static void Main(string[] args) {
+            Listener listener = new Listener("127.0.0.1", 8300);
+            listener.Start();
         }
     }
 }
