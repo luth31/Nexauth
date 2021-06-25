70840a4d825550f03edf4a34d840cfa6a932693e
Paul Munteanu <luth@luthcode.net>
2021-06-25 12:58:25 +0300

    Add a simple use case for payloads

--- nexauth-server/Listener.cs
+++ nexauth-server/Listener.cs
@@ -29,6 +29,27 @@ namespace nexauth_server {
             Console.WriteLine("Awaiting connection...");
             TcpClient client = listener.AcceptTcpClient();
             Console.WriteLine("Client connected!");
+            while (true) {
+                Console.WriteLine("Awaiting CHelloPayload (Opcode 1)");
+                CHelloPayload cpayload = Payload.ReadAs<CHelloPayload>(client.GetStream());
+                Console.WriteLine($"Received payload! Opcode: {cpayload.Opcode}");
+                Console.WriteLine("Sending SHelloPayload... (Opcode 2)");
+                SHelloPayload payload = new SHelloPayload();
+                payload.Send(client.GetStream());
+                Console.WriteLine("Sent SHelloPayload! (Opcode 2)");
+                CSecureReqPayload secure_req = Payload.ReadAs<CSecureReqPayload>(client.GetStream());
+                string pubkey =
+@"-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCbXmvJvECrf641tjoqwD214LAM
+nLNj1tmE6L3MuTIeysRF/P8o1LuhQe9G3hhukwAhuL3opSRxIFmw4CHn++epwk7C
+uW1ix0IRZMreOAVwMNP2a4hPNyRajblShNFTqL78mefqNoz3j1HHUxuk8qZLhPCo
+vvfadeiEn/W99W5EqwIDAQAB
+-----END PUBLIC KEY-----";
+                Console.WriteLine($"Received payload CSecureReqPayload! {secure_req.ToString()}");
+                SSecureResPayload secure_res = new SSecureResPayload(pubkey);
+                Console.WriteLine($"Sending payload SSecureResPayload with key {secure_res.publicKey}");
+                secure_res.Send(client.GetStream());
+            }
         }
 
         private readonly TcpListener listener;
