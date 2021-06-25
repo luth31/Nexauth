b80b7739a9b226669684c1c3220f7afed72053a1
Paul Munteanu <luth@luthcode.net>
2021-06-25 13:54:15 +0300

    Create login window and add Client class

---
+++ nexauth-client/Client.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Text.RegularExpressions;
+using System.Net.Sockets;
+using System.Text.Json;
+using nexauth;
+
+namespace nexauth_client {
+    class Client {
+        public Client(MainWindow parent) {
+            this.parent = parent;
+        }
+
+        public async Task<bool> SetHost(string Address, Int32 Port) {
+            IPAddress addr;
+            parent.UpdateStatusText(parent.STATUS_LABEL, "Looking up host...", Brushes.Gray);
+            parent.ClearStatusText(parent.HOST_STATUS_LABEL);
+            try {
+                IPHostEntry entry = await Dns.GetHostEntryAsync(Address);
+                addr = entry.AddressList[0];
+            }
+            catch (System.Net.Sockets.SocketException e) {
+                parent.UpdateStatusText(parent.HOST_STATUS_LABEL, "Invalid host!", Brushes.Red);
+                return false;
+            }
+            this.address = addr;
+            this.port = Port;
+            return true;
+        }
+
+        public bool SetUsername(string username) {
+            parent.ClearStatusText(parent.USERNAME_STATUS_LABEL);
+            Regex reg = new Regex("^[a-zA-Z0-9]*$");
+            if (reg.IsMatch(username))
+                this.username = username;
+            else {
+                parent.UpdateStatusText(parent.USERNAME_STATUS_LABEL, "Invalid username!", Brushes.Red);
+                return false;
+            }
+            return true;
+        }
+
+        public void Connect() {
+            if (client != null)
+                return;
+            client = new TcpClient();
+            client.Connect(address, port);
+
+        }
+
+        public void Disconnect() {
+            client.Close();
+        }
+
+        public void SendUsername() {
+
+        }
+
+        bool connected;
+        private TcpClient client;
+        private Int32 port;
+        private MainWindow parent;
+        private IPAddress address;
+        private string username;
+    }
+}
--- nexauth-client/MainWindow.xaml.cs
+++ nexauth-client/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,12 +15,46 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace nexauth_client {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            this.LOGIN_BUTTON.Click += loginButton_click;
+            client = new Client(this);
         }
+
+        private async void loginButton_click(object sender, RoutedEventArgs e) {
+            bool success = client.SetUsername(USERNAME_TEXTBOX.Text) & await client.SetHost(HOST_TEXTBOX.Text, 8300);
+            if (!success) {
+                UpdateStatusText(STATUS_LABEL, "There are errors in the input!", Brushes.Red);
+            }
+            else {
+                ClearStatusText(STATUS_LABEL);
+                ClearStatusText(HOST_STATUS_LABEL);
+                ClearStatusText(USERNAME_STATUS_LABEL);
+                client.Connect();
+                client.SendUsername();
+            }
+            
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e) {
+            client.Disconnect();
+        }
+
+        public void UpdateStatusText(Label label, string text) {
+            label.Content = text;
+            label.Foreground = Brushes.Black;
+        }
+
+        public void UpdateStatusText(Label label, string text, Brush color) {
+            label.Content = text;
+            label.Foreground = color;
+        }
+
+        public void ClearStatusText(Label label) {
+            label.Content = "";
+        }
+
+        private Client client;
     }
 }
