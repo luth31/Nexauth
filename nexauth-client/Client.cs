07db92914919828e5790a57a3d688db506c67455
Paul Munteanu <luth@luthcode.net>
2021-06-30 22:00:10 +0300

    Refactor Client.cs

--- nexauth-client/Client.cs
+++ nexauth-client/Client.cs
@@ -59,35 +59,63 @@ namespace nexauth_client {
         }
 
         public void Disconnect() {
-            client.Close();
+            if (client != null)
+                client.Close();
         }
 
-        public async void BeginSecureConnection() {
+        public async void BeginAuth() {
+            try {
+                await Hello();
+                await SecureChannel();
+                await DoAuth();
+            } catch (Exception e) {
+                Debug.WriteLine($"Authentication failed! Reason: {e.Message}");
+                if (client != null) {
+                    client.Close();
+                    client = null;
+                }
+            }
+        }
+
+        private async Task Hello() {
             CHelloPayload chello_payload = new CHelloPayload();
             chello_payload.SendAsync(client);
             SHelloPayload shello_payload = await Payload.ReadAsyncAs<SHelloPayload>(client);
+        }
+
+        private async Task SecureChannel() {
             CBeginSecurePayload csecure_payload = new CBeginSecurePayload();
             csecure_payload.SendAsync(client);
+            parent.UpdateStatusText(parent.STATUS_LABEL, "Exchanging keys...", Brushes.BlueViolet);
             SSendPubkeyPayload spubkey_payload = await Payload.ReadAsyncAs<SSendPubkeyPayload>(client);
             cRSAProvider = new RSACryptoServiceProvider(4096);
             sRSAProvider = new RSACryptoServiceProvider();
             sRSAProvider.FromXmlString(spubkey_payload.publicKey);
             string pubkey = cRSAProvider.ToXmlString(false);
-            CSendPubkeyPayload cpubkey1_payload = new CSendPubkeyPayload(pubkey[..(pubkey.Length/2)]);
-            CSendPubkeyPayload cpubkey2_payload = new CSendPubkeyPayload(pubkey[(pubkey.Length/2)..]);
+            CSendPubkeyPayload cpubkey1_payload = new CSendPubkeyPayload(pubkey[..(pubkey.Length / 2)]);
+            CSendPubkeyPayload cpubkey2_payload = new CSendPubkeyPayload(pubkey[(pubkey.Length / 2)..]);
             cpubkey1_payload.SendEncryptedAsync(client, sRSAProvider);
             cpubkey2_payload.SendEncryptedAsync(client, sRSAProvider);
             SSendAesKeyPayload aes_payload = await EncryptedPayload.ReadAsyncAs<SSendAesKeyPayload>(client, cRSAProvider);
-            Debug.WriteLine($"Received payload with key: {Encoding.UTF8.GetString(aes_payload.key)}");
             aesProvider = new AESProvider(AESProvider.AES_KEY_SIZE.AES_KEY_128, aes_payload.key, aes_payload.nonce);
+        }
+
+        private async Task DoAuth() {
             CBeginAuthPayload cauth_payload = new CBeginAuthPayload(username);
             cauth_payload.SendEncryptedAsync(client, aesProvider);
             SAuthStandbyPayload sauth_payload = await EncryptedPayload.ReadAsyncAs<SAuthStandbyPayload>(client, aesProvider);
             parent.UpdateStatusText(parent.STATUS_LABEL, "Awaiting authentication...", Brushes.Orange);
-            SAuthSuccessPayload slogged_payload = await EncryptedPayload.ReadAsyncAs<SAuthSuccessPayload>(client, aesProvider);
-            parent.UpdateStatusText(parent.STATUS_LABEL, "Authenticated!", Brushes.Green);
-            MessageBox.Show("Authentication successful!");
+            SAuthNewStatePayload slogged_payload = await EncryptedPayload.ReadAsyncAs<SAuthNewStatePayload>(client, aesProvider);
+            if (slogged_payload.success) {
+                parent.UpdateStatusText(parent.STATUS_LABEL, "Authenticated!", Brushes.Green);
+                MessageBox.Show($"Authentication successful! ({slogged_payload.message})");
+            }
+            else {
+                parent.UpdateStatusText(parent.STATUS_LABEL, "Unauthenticated", Brushes.Red);
+                MessageBox.Show($"Authentication failed! Reason: {slogged_payload.message}");
+            }
             client.Close();
+            client = null;
         }
 
         private AESProvider aesProvider;
