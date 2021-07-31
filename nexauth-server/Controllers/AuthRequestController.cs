5f46fa756a6868de9260c67e36930367399ef3bc
Paul Munteanu <luth@luthcode.net>
2021-07-31 17:17:36 +0300

    Implement signature verification in AuthRequestController

--- nexauth-server/Controllers/AuthRequestController.cs
+++ nexauth-server/Controllers/AuthRequestController.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using nexauth_server.Models;
@@ -21,14 +18,28 @@ namespace nexauth_server.Controllers
         }
 
         // GET: api/AuthRequest
-        [HttpGet]
+        /*[HttpGet]
         public async Task<ActionResult<IEnumerable<AuthRequest>>> GetAuthRequests()
         {
             return await _context.AuthRequests.ToListAsync();
+        }*/
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AuthRequest>> GetRequestForUser(string id) {
+            var user = await _context.User.Where(u => u.Username == id).FirstOrDefaultAsync();
+            if (user == null) {
+                return NotFound();
+            }
+            var authRequest = await _context.AuthRequests.Where(r => r.UserId == user.Id).FirstOrDefaultAsync();
+            if (authRequest == null) {
+                return NotFound();
+            }
+
+            return authRequest;
         }
 
         // GET: api/AuthRequest/5
-        [HttpGet("{id}")]
+        /*[HttpGet("{id}")]
         public async Task<ActionResult<AuthRequest>> GetAuthRequest(long id)
         {
             var authRequest = await _context.AuthRequests.FindAsync(id);
@@ -39,11 +50,11 @@ namespace nexauth_server.Controllers
             }
 
             return authRequest;
-        }
+        }*/
 
         // PUT: api/AuthRequest/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
+        /*[HttpPut("{id}")]
         public async Task<IActionResult> PutAuthRequest(long id, AuthRequest authRequest)
         {
             if (id != authRequest.Id)
@@ -70,28 +81,39 @@ namespace nexauth_server.Controllers
             }
 
             return NoContent();
-        }
+        }*/
 
         // POST: api/AuthRequest
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<AuthRequest>> PostAuthRequest(AuthRequest authRequest)
+        public async Task<ActionResult<AuthRequest>> PostAuthRequest([FromBody] AuthResponse response)
         {
-            _context.AuthRequests.Add(authRequest);
-            await _context.SaveChangesAsync();
+            var user = await _context.User.FindAsync(response.userId);
+            var authReq = await _context.AuthRequests.FindAsync(response.reqId);
+            if (user.Id != authReq.UserId)
+                return BadRequest();
+            bool verify = ECDSAProvider.VerifySignature(user.Key, authReq.Challenge, response.signedChallenge);
+            if (verify == true) {
+                authReq.Completed = true;
+                _context.Entry(authReq).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                return NoContent();
+            }
+            else
+                return NotFound();
 
-            return CreatedAtAction("GetAuthRequest", new { id = authRequest.Id }, authRequest);
         }
 
         // DELETE: api/AuthRequest/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteAuthRequest(long id)
+        public async Task<IActionResult> DeleteAuthRequest(string id)
         {
-            var authRequest = await _context.AuthRequests.FindAsync(id);
-            if (authRequest == null)
-            {
+            var user = await _context.User.Where(u => u.Username == id).FirstOrDefaultAsync();
+            if (user == null)
                 return NotFound();
-            }
+            var authRequest = await _context.AuthRequests.Where(a => a.UserId == user.Id).FirstOrDefaultAsync();
+            if (authRequest == null)
+                return NotFound();
 
             _context.AuthRequests.Remove(authRequest);
             await _context.SaveChangesAsync();
