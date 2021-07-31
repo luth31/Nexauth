c1ea04d7badcbe1a0a224c366b4d810f98ee58cd
Paul Munteanu <luth@luthcode.net>
2021-07-31 15:42:28 +0300

    Create AuthResponse model

---
+++ nexauth-server/Models/AuthResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nexauth_server.Models {
+    public class AuthResponse {
+        public long reqId { get; set; }
+        public long userId { get; set; }
+        public string challenge { get; set; }
+        public string signedChallenge { get; set; }
+
+    }
+}
