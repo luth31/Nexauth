29ff69771864f982b1cbdb1bdcff835bd43ce02b
Paul Munteanu <luth@luthcode.net>
2021-09-14 18:12:16 +0300

    [Packet] Add opcodes

---
+++ Packets/Opcode.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Nexauth.Networking {
+    public enum Opcode : Int32 {
+        NULL = 0,
+        CLIENT_HELLO = 1,
+        SERVER_HELLO = 2,
+        CLIENT_BEGIN_SECURE = 3,
+        SERVER_SEND_PUBKEY = 4,
+        CLIENT_SEND_PUBKEY = 5,
+        SERVER_SEND_AESKEY = 6,
+        CLIENT_BEGIN_AUTH = 7,
+        SERVER_AUTH_STANDBY = 8,
+        SERVER_AUTH_STATE_CHANGED = 9
+    }
+}
