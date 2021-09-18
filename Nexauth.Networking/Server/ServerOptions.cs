90fb4f70c78f2994c7bbd87cc5d9bec18052df3f
Paul Munteanu <luth@luthcode.net>
2021-09-18 20:29:02 +0300

    [Networking/Server] Add MaxClients to ServerOptions

--- Nexauth.Networking/Server/ServerOptions.cs
+++ Nexauth.Networking/Server/ServerOptions.cs
@@ -1,9 +1,10 @@
 using System;
 
-namespace Nexauth.Networking.Server {
+namespace Nexauth.Networking {
     public class ServerOptions {
         public string Address { get; set; } = "127.0.0.1";
 
         public ushort Port { get; set; } = 8300;
+        public int MaxClients { get; set; } = 100;
     }
 }
