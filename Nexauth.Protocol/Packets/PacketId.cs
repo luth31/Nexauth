35bdbb7111f98060a66a4068d86d17d72d574bff
Paul Munteanu <luth@luthcode.net>
2021-09-25 18:29:19 +0300

    [Protocol/Packet] Remove Packets.cs, replace it with PacketId

---
+++ Nexauth.Protocol/Packets/PacketId.cs
@@ -0,0 +1,8 @@
+namespace Nexauth.Protocol.Packets {
+    public enum PacketId : ushort {
+        CLIENT_HELLO,
+        CLIENT_DISCONNECT,
+        SERVER_DISCONNECT,
+        SERVER_WELCOME,
+    }
+}
