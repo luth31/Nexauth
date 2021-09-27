14249157685ad648ca0207f9d3d443908052d82c
Paul Munteanu <luth@luthcode.net>
2021-09-28 02:06:40 +0300

    [Protocol/Client] Implement basic Client

---
+++ Nexauth.Protocol/Client/Client.cs
@@ -0,0 +1,33 @@
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace Nexauth.Protocol {
+    public class Client {
+        public Client(ILogger<Client> Logger, ClientOptions Options = null) {
+            if (Options != null) {
+                _options = new ClientOptions();
+            }
+            else {
+                _options = Options;
+            }
+            _cancellationTokenSource = new CancellationTokenSource();
+            _client = new TcpClient();
+        }
+
+        public void Connect() {
+            
+        }
+
+        public async Task HandleConnectionAsync(CancellationToken Token) {
+            while (true) {
+                
+            }
+        }
+
+        CancellationTokenSource _cancellationTokenSource;
+        TcpClient _client;
+        ClientOptions _options;
+    }
+}
