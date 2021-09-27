475dd8a99c3804e5be641ee8831db35f611359b8
Paul Munteanu <luth@luthcode.net>
2021-09-28 02:03:06 +0300

    [Protocol/Version] Make functions static; add missing return to IsSupported(string); catch exceptions and return false in IsSupported

--- Nexauth.Protocol/Version.cs
+++ Nexauth.Protocol/Version.cs
@@ -4,40 +4,46 @@ using System.Linq;
 namespace Nexauth.Protocol {
     public static class Version {
 
-        public bool IsSupported(string Version) {
-            var versionArray = Version.Split('.').Select(int.Parse).ToArray();
-            if (versionArray.Length == 3) {
-                int major, minor, patch = versionArray;
-                IsSupported(major, minor, patch);
+        public static bool IsSupported(string Version) {
+            try {
+                int[] versionArray = Version.Split('.').Select(int.Parse).ToArray();
+                if (versionArray.Length == 3) {
+                    int major = versionArray[0];
+                    int minor = versionArray[1];
+                    int patch = versionArray[2];
+                    return IsSupported(major, minor, patch);
+                }
+            } catch {
+                return false;
             }
             return false;
         }
 
-        public bool IsSupported(int Major, int Minor, int Patch) {
-            if (this.Major != Major || this.Minor != Minor)
+        public static bool IsSupported(int Major, int Minor, int Patch) {
+            if (Version.Major != Major || Version.Minor != Minor)
                 return false;
             return true;
         }
 
-        public int Major {
+        public static int Major {
             get {
                 return 0;
             }
         }
 
-        public int Minor {
+        public static int Minor {
             get {
                 return 1;
             }
         }
 
-        public int Patch {
+        public static int Patch {
             get {
                 return 0;
             }
         }
 
-        public string VersionString {
+        public static string VersionString {
             get {
                 return String.Concat(Major, ".", Minor, ".", Hotfix);
             }
