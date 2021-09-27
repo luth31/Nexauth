42306f684e4c5c853f2012ab25144e3971411c5a
Paul Munteanu <luth@luthcode.net>
2021-09-28 02:17:35 +0300

    [Tests][Protocol/Version] Implement IsSupported_CurrentVersion_ReturnsTrue, IsSupported_BadFormat_ReturnsFalse, IsSupported_VersionOverflow_ReturnsFalse tests for Version

---
+++ Nexauth.Protocol.Tests/VersionTest.cs
@@ -0,0 +1,31 @@
+using Xunit;
+using Nexauth.Protocol;
+
+namespace Nexauth.Protocol.Tests {
+        public class VersionTest {
+
+        [Fact]
+        public void IsSupported_CurrentVersion_ReturnsTrue() {
+            // Act
+            bool supported = Version.IsSupported("0.1.0");
+            // Assert
+            Assert.True(supported);
+        }
+        
+        [Fact]
+        public void IsSupported_BadFormat_ReturnsFalse() {
+            // Act
+            bool supported = Version.IsSupported("a.1.0");
+            // Assert
+            Assert.False(supported);
+        }
+
+        [Fact]
+        public void IsSupported_VersionOverflow_ReturnsFalse() {
+            // Act
+            bool supported = Version.IsSupported("10000000000000000000000000.1.0");
+            // Assert
+            Assert.False(supported);
+        }
+    }
+}
